ff3c6ecbb66da4e14a73126a9deff88b417da458
Yongye Tan <yongyetan1209@gmail.com>
2024-05-01 11:51:21 -0400

    Initial commit

---
+++ CupGame/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CupGame
+//
+//  Created by local-user on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CupGameView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupGame/CupGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupGameApp.swift
+//  CupGame
+//
+//  Created by local-user on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CupGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
