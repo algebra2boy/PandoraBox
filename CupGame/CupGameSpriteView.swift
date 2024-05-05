84927f25b04fbaccb49f9924c5e8701256afef3a
Yongye Tan <yongyetan1209@gmail.com>
2024-05-05 11:07:32 -0400

    endpoint fix and add repeated sequence

--- CupGame/CupGameSpriteView.swift
+++ CupGame/CupGameSpriteView.swift
@@ -138,8 +138,9 @@ extension CupGameScene {
         let moveUp = SKAction.moveBy(x: 0, y: 100, duration: 1)
         let moveDown = SKAction.moveBy(x: 0, y: -100, duration: 1)
         let sequence = SKAction.sequence([moveUp, moveDown])
+        let repeated_seq = SKAction.repeat(sequence, count: 2)
         
-        return sequence
+        return repeated_seq
     }
     
     
@@ -150,7 +151,7 @@ extension CupGameScene {
         let startPoint = CGPoint(x: 300, y: 200)
         let endPoint = CGPoint(x: 700, y: 200)
         
-        let control1 = CGPoint(x: 450, y: 150)
+        let control1 = CGPoint(x: 500, y: 400)
         let control2 = CGPoint(x: 700, y: 200)
         
         path.move(to: startPoint)
@@ -162,11 +163,11 @@ extension CupGameScene {
         let startPoint2 = CGPoint(x: 700, y: 200)
         let endPoint2 = CGPoint(x: 300, y: 200)
         
-        let control3 = CGPoint(x: 500, y: 400)
+        let control3 = CGPoint(x: 500, y: 0)
         
         // the last control cannot be the exact point as endpoint 2
         // otherwise it causes endpoint discrepancies
-        let control4 = CGPoint(x: 300, y: 199)
+        let control4 = CGPoint(x: 299, y: 199)
         
         path2.move(to: startPoint2)
         path2.addCurve(to: endPoint2, control1: control3, control2: control4)
@@ -174,16 +175,17 @@ extension CupGameScene {
         let curve2 = SKAction.follow(path2, asOffset: false, orientToPath: false, duration: 1)
         
         let sequence = SKAction.sequence([curve, curve2])
+        let repeated_seq = SKAction.repeat(sequence, count: 2)
         
-        return sequence
+        return repeated_seq
         
     }
     
     func getRightCupAction(cup: SKSpriteNode) -> SKAction {
         
         let path = CGMutablePath()
-                
-        let startPoint = CGPoint(x: 700, y: 200)
+        
+        let startPoint = CGPoint(x: 699, y: 200)
         let endPoint = CGPoint(x: 300, y: 200)
         
         let control1 = CGPoint(x: 500, y: 0)
@@ -195,11 +197,14 @@ extension CupGameScene {
         let curve = SKAction.follow(path, asOffset: false, orientToPath: false, duration: 1)
         
         let path2 = CGMutablePath()
-        let startPoint2 = CGPoint(x: 300, y: 200 )
+        let startPoint2 = CGPoint(x: 300, y: 200)
         let endPoint2 = CGPoint(x: 700, y: 200)
         
-        let control3 = CGPoint(x: 500, y: 400)
-        let control4 = CGPoint(x: 300, y: 199)
+        let control3 = CGPoint(x: 500, y: 420)
+        
+        // the last control cannot be the exact point as endpoint 2
+        // otherwise it causes endpoint discrepancies
+        let control4 = CGPoint(x: 699, y: 199)
         
         path2.move(to: startPoint2)
         path2.addCurve(to: endPoint2, control1: control3, control2: control4)
@@ -207,8 +212,9 @@ extension CupGameScene {
         let curve2 = SKAction.follow(path2, asOffset: false, orientToPath: false, duration: 1)
         
         let sequence = SKAction.sequence([curve, curve2])
+        let repeated_seq = SKAction.repeat(sequence, count: 2)
         
-        return sequence
+        return repeated_seq
         
     }
 }
